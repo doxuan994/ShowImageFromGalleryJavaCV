16a3316aefb5968a567479bd1a8d44db6f2aa91f
Xuan Do <doxuancntp@gmail.com>
2018-06-13 13:16:56 -0400

    Added Face Detection!!!

--- app/src/main/java/com/doxua/www/showimagefromgalleryjavacv/MainActivity.java
+++ app/src/main/java/com/doxua/www/showimagefromgalleryjavacv/MainActivity.java
@@ -1,8 +1,10 @@
 package com.doxua.www.showimagefromgalleryjavacv;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.database.Cursor;
 import android.graphics.Bitmap;
+import android.media.FaceDetector;
 import android.net.Uri;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
@@ -11,37 +13,55 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import org.bytedeco.javacpp.opencv_core;
-import org.opencv.core.Mat;
+import org.bytedeco.javacpp.opencv_objdetect;
+import org.bytedeco.javacv.Frame;
+import org.bytedeco.javacpp.opencv_core.Mat;
 import org.opencv.imgcodecs.Imgcodecs;
 
 
-//import org.opencv.android.Utils;
+import org.bytedeco.javacv.AndroidFrameConverter;
+import org.bytedeco.javacv.OpenCVFrameConverter;
+import org.w3c.dom.Text;
 
 import java.io.File;
+import java.io.IOException;
+
+import static android.view.View.INVISIBLE;
+import static android.view.View.VISIBLE;
+import static org.bytedeco.javacpp.opencv_imgproc.CV_BGR2GRAY;
+import static org.bytedeco.javacpp.opencv_imgproc.cvtColor;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final String TAG = "MainActivity";
+
+
     private static final int PICK_IMAGE = 100;
     private ImageView imageView;
+    private TextView textView;
+
+    private opencv_objdetect.CascadeClassifier faceDetector;
+    private int absoluteFaceSize = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Call the image view tp display the selected image from the gallery.
-        imageView = (ImageView) findViewById(R.id.image_view);
-        Button pickImageButton = (Button) findViewById(R.id.pick_image_button);
+        // Create the image view and text view.
+        imageView = (ImageView) findViewById(R.id.imageView);
+        textView = (TextView) findViewById(R.id.faces_value);
+
+        Button pickImageButton = (Button) findViewById(R.id.btnGallery);
         pickImageButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 openGallery();
             }
         });
-
-
     }
 
     // Add-on method
@@ -58,47 +78,62 @@ public class MainActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (resultCode == RESULT_OK && requestCode == PICK_IMAGE) {
+
+
+
             Uri imageUri = data.getData();
 
-
-            // Convert to OpenCV.
-
-            String[] filePathColumn = { MediaStore.Images.Media.DATA };
-
-            // Get the cursor
-            Cursor cursor = getContentResolver().query(imageUri,
-                    filePathColumn, null, null, null);
-            // Move to first row
-            cursor.moveToFirst();
-
-            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
-            String imgDecodableString = cursor.getString(columnIndex);
-            cursor.close();
-            File file = new File(imgDecodableString);
-
-            Log.e(getString(R.string.app_name), "File exists: " + file.exists());
-            Log.e(getString(R.string.app_name), "Trying to read: " + file.getAbsolutePath());
-            Mat image = Imgcodecs.imread(file.getAbsolutePath(), Imgcodecs.CV_LOAD_IMAGE_COLOR);
-            Bitmap resultBitmap = Bitmap.createBitmap(image.cols(), image.rows(), Bitmap.Config.ARGB_8888);;
-            // Converts OpenCV Mat to Android Bitmap.
-            // Utils.matToBitmap(image, resultBitmap);
-            Bitmap mResult = resultBitmap;
-
-            imageView.setImageBitmap(mResult);
-
+            // Convert to Bitmap.
+            Bitmap bitmap = null;
+            try {
+                bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), imageUri);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
 
 
 
             // Display the image.
+            imageView.setImageBitmap(bitmap);
+            // Detect faces and display number of faces in the text view.
+            detectAndDisplay(bitmap, textView);
 
 
 
-
-            // imageView.setImageURI(imageUri);
         }
     }
 
+    void detectAndDisplay(Bitmap image, TextView facesValue) {
+
+        AndroidFrameConverter converterToBitmap = new AndroidFrameConverter();
+        OpenCVFrameConverter.ToMat converterToMat = new OpenCVFrameConverter.ToMat();
+
+        // Convert to Bitmap.
+        Frame frame = converterToBitmap.convert(image);
+        // Convert to Mat.
+        Mat mat = converterToMat.convert(frame);
+
+        // Load the CascadeClassifier class to detect objects.
+        faceDetector = TrainHelper.loadClassifierCascade(MainActivity.this, R.raw.frontalface);
 
 
 
+        // Convert to Gray scale.
+        cvtColor(mat, mat, CV_BGR2GRAY);
+        // Vector of rectangles where each rectangle contains the detected object.
+        opencv_core.RectVector faces = new opencv_core.RectVector();
+
+        // Detect the face.
+        faceDetector.detectMultiScale(mat, faces, 1.25f, 3, 1,
+                new opencv_core.Size(absoluteFaceSize, absoluteFaceSize),
+                new opencv_core.Size(4 * absoluteFaceSize, 4 * absoluteFaceSize));
+
+
+        // Count number of faces and display in text view.
+        int numFaces = (int) faces.size();
+        // String s = Integer.toString(numFaces);
+
+        facesValue.setText("" + numFaces);
+
+    }
 }
